737644eef556a451d40469bff14d527627b2e3a3
Shubhakar <Saishubhakar97@gmail.com>
2022-07-12 21:18:32 +0530

    The porject has been completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import Navbar from './addfactors/Navbar';
+import News from './addfactors/News';
+import LoadingBar from 'react-top-loading-bar'
+import {BrowserRouter as Router,Switch,Route,} from "react-router-dom"
+
+const App  = ()=> {
+  const apiKey = process.env.REACT_APP_NEWS_API
+  const pageSize = 6;
+
+  const [progress, setProgress] = useState(0)
 
 
-export default class App extends Component {
-  name = "Shubhakar"
-  render() {
     return (
       <div>
-        Jodo ima navota toko jaki navote neyo {this.name}
+        <Router>
+          <LoadingBar
+          height={2.5}
+            color='#f11946'
+            progress={progress}
+          />
+          <Navbar />
+          <Switch>
+            <Route exact path="/"><News apiKey = {apiKey} setProgress = {setProgress} key="general" pageSize={pageSize} country="in" category="general" /></Route>
+            <Route exact path="/business"><News apiKey = {apiKey} setProgress = {setProgress} key="business" pageSize={pageSize} country="in" category="business" /></Route>
+            <Route exact path="/entertainment"><News apiKey = {apiKey} setProgress = {setProgress} key="entertainment" pageSize={pageSize} country="in" category="entertainment" /></Route>
+            <Route exact path="/science"><News apiKey = {apiKey} setProgress = {setProgress} key="science" ppageSize={pageSize} country="in" category="science" /></Route>
+            <Route exact path="/sports"><News apiKey = {apiKey} setProgress = {setProgress} key="sports" pageSize={pageSize} country="in" category="sports" /></Route>
+            <Route exact path="/technology"><News apiKey = {apiKey} setProgress = {setProgress} key="technology" pageSize={pageSize} country="in" category="technology" /></Route>
+          </Switch>
+        </Router>
       </div>
     )
   }
-}
 
+export default App
